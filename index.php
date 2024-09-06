922c47704e4ca28d8af9f9a8dcfbe8178a1baea0
RYAN CLAIRE ORTEGA <127005371+RyanClaire@users.noreply.github.com>
2024-09-06 18:45:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/edd0240440.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="finance.js"></script>
+    <title>Finance Dashboard</title>
+</head>
+
+<body>
+<div id="sidebar">
+    <h2>Financial Dashboard</h2>
+    <ul>
+      <li><a href="adminHome.php"><i class="fas fa-home"></i> Home</a></li>
+      <li><a href="adminAnalytics.php"><i class="fas fa-chart-line"></i> Disbursement</a></li>
+      <li><a href="adminUser.php"><i class="fas fa-copy"></i> Budget Management</a></li>
+      <li><a href="adminAM.php"><i class="fas fa-money-bill"></i> Collection</a>
+      <li><a href="signup.php"><i class="fas fa-address-book"></i> General Ledger</a></li>
+      <li><a href="signup.php"><i class="fas fa-file-invoice"></i> Account Payable/Recievable</a></li> 
+    </ul>
+  </div>
+ 
+
+<div id="content">
+
+<header>
+<button id="menu-btn" onclick="toggleSidebar()"><i class="fas fa-bars"></i></button>
+<button>
+</header>
+
+
+    <h1>Admin Dashboard - Home</h1>
+    <p>Welcome to the Financial Department! This department serves as the cornerstone of our organization, responsible for managing all financial processes and decisions. Our primary goal is to ensure the effective allocation of resources, maintain financial stability, and support the overall strategic objectives of the company.</p>
+
+    <h2>Important Statistics</h2>
+    <ul>
+      <li>Perang nanakaw: 200000</li>
+      <li>Perang naitago: 0</li>
+      <li>Imissyou: 143</li>
+    </ul>
+    <h2>Recent Notifications</h2>
+    <p>1. Miss na kita</p>
+    <p>2. Miss na kita balik please</p>
+    <p>3. balik na ikaw ehehe</p>
+    <p>Miss na ni aco gf nya</p>
+    <p>Miss na ni ken ex nya, he want her back na :bb</p>
+    <p>ken love jane</p>
+    <h1> KEN LOVE JANE <h1>
+      <h1> AERON WALANG GF, 5 YEARS NA <h1>
+
+
+</div>
+<footer>
+      <p>&copy; 2024 Financial Department | All rights reserved</p>
+</footer>
+</body>
+
+</html>
